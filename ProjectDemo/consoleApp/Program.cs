558d45af239e4960e62b14c867e7bab16da2ec06
Xuan Nguyen <nguyenthihaxuan040900hiie@gmail.com>
2020-10-01 16:07:31 +0700

    commit first

--- ProjectDemo/consoleApp/Program.cs
+++ ProjectDemo/consoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,21 @@ namespace consoleApp
         {
             Console.WriteLine("Hello World From C#");
             Console.WriteLine("Press any key to quit");
+            int s = 0;
+            for(int i = 1; i <= 7; i++)
+            {
+                s += i;
+            }
+            Console.WriteLine($"sum of 1 to 7 = {s}");
+            increase(ref s);
+            Console.WriteLine($"value of s after increase = {s}");
             Console.ReadKey();
             // Delegate is a pointer to a function
+        }
+
+        static void increase(ref int s)
+        {
+            s++;
         }
     }
 }
